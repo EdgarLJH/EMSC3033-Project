75a862733dbdaf43fa7caa8b804273df2eba83fd
EdgarLJH <103945325+EdgarLJH@users.noreply.github.com>
2022-05-29 17:09:10 +1000

    Update dependencies.py

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,2 +1,16 @@
 """Import dependencies for this notebook"""
 
+import os
+import io   
+import netCDF4
+import random
+import numpy as np
+import pandas as pd
+
+import pygmt
+import matplotlib
+import matplotlib.pyplot as plt
+
+from matplotlib import colorbar, colors
+from global_land_mask import globe
+from scipy import stats
